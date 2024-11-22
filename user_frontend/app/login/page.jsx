fc9a4ecea5595903dd9ea9c95a2dbcebdbf505f8
paulybizz <eaa24carp@students.eaaa.dkgit config --global user.emailgit config --global user.emailgit config --global user.email>
2024-11-22 15:00:16 +0100

    Refactor login page: extract login form into a separate component for better organization and maintainability

--- user_frontend/app/login/page.jsx
+++ user_frontend/app/login/page.jsx
@@ -1,28 +1,8 @@
-"use client";
-import React, { useState } from "react";
-import { Button, Input } from "@/global/components";
-import { login } from "@/actions";
-import Image from "next/image";
+import LoginForm from "@/global/components/loginForm"
+
+import Image from "next/image"
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const result = await login(email, password);
-
-    if (result.error) {
-      console.error("Error:", result.error);
-    } else {
-      console.log("Succes", result.data);
-    }
-
-    console.log("Email:", email);
-    console.log("Password:", password);
-  };
-
   return (
     <div>
       <div className="mx-auto w-[70%] max-w-[70.5rem]">
@@ -45,61 +25,9 @@ export default function LoginPage() {
         >
           Welcome back
         </h2>
-        <div className="max-w-xs px-4 py-4 bg-white border border-gray-200 shadow-sm rounded-md">
-          <form className="space-y-4 text-xs" onSubmit={handleSubmit}>
-            <div>
-              <label
-                htmlFor="email"
-                className="block  font-medium text-gray-700"
-              >
-                Email
-              </label>
-              <Input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                placeholder=""
-                required
-                value={email}
-                onChange={(value) => setEmail(value)}
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="password"
-                className="block font-medium text-gray-700"
-              >
-                Password
-              </label>
-              <Input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                placeholder=""
-                required
-                value={password}
-                onChange={(value) => setPassword(value)}
-              />
-            </div>
-            <div className="flex justify-center">
-              <Button type="submit" title="Login"></Button>
-            </div>
-            <div className="text-center mt-4">
-              <p className="text-xs text-gray-600">
-                Don't have an account?{" "}
-                <a
-                  href="/create-user"
-                  className="text-blue-600 hover:underline"
-                >
-                  Create one
-                </a>
-              </p>
-            </div>
-          </form>
-        </div>
+
+        <LoginForm />
       </div>
     </div>
-  );
+  )
 }
