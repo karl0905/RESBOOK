a223021b662dd50137d67ad6880a61a553b1a86f
karl0905 <kalle44@live.dk>
2024-11-22 20:37:51 +0100

    added refresh/index endpoint to refresh access_token based on refresh_token

--- backend/api/auth/refresh.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Endpoint to refresh the user's token using the refresh token as parameter
-include($_SERVER["DOCUMENT_ROOT"] . "/functions/handleApiRequest.php");
-
-handle_api_request('POST');
-
-
