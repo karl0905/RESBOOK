e4a3b15e5e654393d2e58812e136db5dec6bd271
madsnbkristensen <madskristensen@hotmail.dk>
2024-11-29 12:24:44 +0100

    Booking delete endpoint created and is_admin function altered

--- backend/functions/is_res_admin.php
+++ backend/functions/is_res_admin.php
@@ -1,9 +1,16 @@
 <?php
 
-function is_res_admin($mySQL, $id) {
-    // select the restaurant_id from the restaturant where id is equal to admin_id
-    $stmt = $mySQL->prepare("SELECT ID FROM restaurant_info WHERE admin_id = ?");
-    $stmt->bind_param("i", $id);
+function is_res_admin($mySQL, $id, $restaurant_id = null) {
+    if ($restaurant_id) {
+        // Check if the user is the admin of the specific restaurant
+        $stmt = $mySQL->prepare("SELECT ID FROM restaurant_info WHERE admin_id = ? AND ID = ?");
+        $stmt->bind_param("ii", $id, $restaurant_id);
+    } else {
+        // Check if the user is the admin of any restaurant
+        $stmt = $mySQL->prepare("SELECT ID FROM restaurant_info WHERE admin_id = ?");
+        $stmt->bind_param("i", $id);
+    }
+    
     $stmt->execute();
     $result = $stmt->get_result();
     $stmt->close();
