3cb36d5d115ef4a1b5d6f4dbb95cb3b12558896e
madsnbkristensen <madskristensen@hotmail.dk>
2024-12-03 11:11:11 +0100

    Create booking stored procedure altered to handle the booking duration. Creating booking endpoint has a small fix, with the is_res_admin functions return.

--- backend/api/bookings/createBooking.php
+++ backend/api/bookings/createBooking.php
@@ -6,9 +6,15 @@ $input = handle_api_request('POST');
 
 $id = authorize($mySQL);
 
+if (!isset($input['restaurant_id'])) {
+    http_response_code(400);
+    echo json_encode(['error' => 'Missing restaurant_id']);
+    exit();
+}
+
 // include the is_res_admin function
 include($_SERVER["DOCUMENT_ROOT"] . "/functions/is_res_admin.php");
-$is_admin = is_res_admin($mySQL, $id);
+$is_admin = is_res_admin($mySQL, $id, $input['restaurant_id']);
 
 if ($is_admin) {
     if(!isset($input['restaurant_id']) || !isset($input['date']) || !isset($input['time']) || !isset($input['guest_count']) || !isset($input['booking_email'])) {
