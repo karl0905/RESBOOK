47131ba39d9b3487a5b2e08dd26e89ee5a611cc0
paulybizz <eaa24carp@students.eaaa.dkgit config --global user.emailgit config --global user.emailgit config --global user.email>
2024-12-04 10:59:44 +0100

    Refactor dashboard components: integrate Logo and Darkbackground, update restaurant display with Card component

--- res_frontend/features/dashboard/Card.jsx
+++ res_frontend/features/dashboard/Card.jsx
@@ -1,4 +1,12 @@
-import { fetchRestaurant } from "../../actions/restaurant.js";
+import React from "react"
 
-export default async function Card() {
+function Card({ restaurant }) {
+  return (
+    <div className="text-white w-full h-auto p-6 rounded-t-2xl bg-gray-800 mb-4">
+      <h2 className="text-xl font-bold">{restaurant.name}</h2>
+      <p>{restaurant.description}</p>
+    </div>
+  )
 }
+
+export default Card
