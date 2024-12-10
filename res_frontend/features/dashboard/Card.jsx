619631139062ff76bd1c7b35d7c876af24e197ac
paulybizz <eaa24carp@students.eaaa.dkgit config --global user.emailgit config --global user.emailgit config --global user.email>
2024-12-10 11:04:41 +0100

    Refactor Card components: enhance styling, update font, and improve layout in dashboard

--- res_frontend/features/dashboard/Card.jsx
+++ res_frontend/features/dashboard/Card.jsx
@@ -3,12 +3,17 @@ import React from "react"
 export default function Card({ name, address, onClick }) {
   return (
     <div
-      className="bg-gray-600 w-[350px] p-4 rounded-lg shadow-md flex-grow relative min-h-[200px]"
+      style={{
+        fontFamily: "'Monda', sans-serif", // Apply Monda font
+      }}
+      className="bg-card-gray p-4 rounded-xl shadow-md flex-grow relative w-80 h-80 cursor-pointer"
       onClick={onClick}
     >
       <div className="absolute top-2 right-2 text-sm text-white">{address}</div>
       <div className="flex flex-col items-center justify-center h-full">
-        <h2 className="text-xl font-bold mb-2 text-center">{name}</h2>
+        <h2 className="text-xl font-bold mb-2 text-center text-white">
+          {name}
+        </h2>
       </div>
     </div>
   )
