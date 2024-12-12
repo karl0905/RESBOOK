d2922173c761ce40564b7f3b8cff9f092fc0005e
karl0905 <kalle44@live.dk>
2024-12-12 14:06:34 +0100

    removed user agent from wrong call

--- res_frontend/app/routes/login.jsx
+++ res_frontend/app/routes/login.jsx
@@ -4,9 +4,6 @@ import { LoginForm } from "../../global/components/loginForm"
 import { login } from "../../actions/user"
 import { set_cookie } from "../../actions/cookie"
 
-// custom user agent for request 
-const customUserAgent = "MinUserAgent/1.0"
-
 // Loader function to handle GET requests
 export const loader = async () => {
   console.log("Loader function called") // Log when loader is triggered
@@ -31,7 +28,6 @@ export const action = async ({ request }) => {
         status: 400,
         headers: { 
           "Content-Type": "application/json",
-          "user-agent": customUserAgent,
         },
       })
     }
