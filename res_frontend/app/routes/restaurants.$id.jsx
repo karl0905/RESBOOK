8ae56decf7f12ca9b067af6cc9310a34ee69127c
paulybizz <eaa24carp@students.eaaa.dkgit config --global user.emailgit config --global user.emailgit config --global user.email>
2024-12-12 11:07:44 +0100

    Refactor fetchRestaurant function and add updateRestaurant function for restaurant data management

--- res_frontend/app/routes/restaurants.$id.jsx
+++ res_frontend/app/routes/restaurants.$id.jsx
@@ -1,10 +1,11 @@
 import { json } from "@remix-run/node"
-import { useLoaderData } from "@remix-run/react"
-import { fetchRestaurant } from "../../actions/restaurants.js"
+import { useLoaderData, useActionData } from "@remix-run/react"
+import { fetchRestaurant, updateRestaurant } from "../../actions/restaurants.js"
 import Darkbackground from "../../features/dashboard/Darkbackground"
 import Logo from "../../features/dashboard/Logo"
 import tonniImage from "../../global/assets/tonni.jpg" // Import the image
 import React, { useState } from "react"
+import { Form } from "@remix-run/react"
 
 export async function loader({ params, request }) {
   try {
@@ -21,142 +22,199 @@ export async function loader({ params, request }) {
   }
 }
 
-export default function RestaurantDetails() {
-  const { restaurant, error } = useLoaderData()
-  const [name, setName] = useState(restaurant.name || "Placeholder name")
-  const [address, setAddress] = useState(
-    restaurant.address || "Placeholder Address"
-  )
-  const [description, setDescription] = useState(
-    restaurant.description || "Placeholder Beskrivelse"
-  )
-  const [email, setEmail] = useState(restaurant.email || "Placeholder@Email.dk")
-  const [capacity, setCapacity] = useState(
-    restaurant.capacity || "Placeholder Kapacitet"
-  )
-  const [phone, setPhone] = useState(restaurant.phone || "Placeholder Telefon")
+export async function action({ request }) {
+  const formData = await request.formData()
+  const method = request.method
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    // Add your update logic here, e.g., make an API call to update the restaurant data
+  if (method === "PUT") {
+    const restaurantData = {
+      id: formData.get("id"),
+      name: formData.get("name"),
+      phone: formData.get("phone"),
+      email: formData.get("email"),
+      address: formData.get("address"),
+      rating: parseFloat(formData.get("rating")),
+      capacity: parseInt(formData.get("capacity")),
+      description: formData.get("description"),
+      booking_duration: parseInt(formData.get("booking_duration")),
+    }
+
     try {
-      const response = await fetch(`/restaurants/${restaurant.id}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name,
-          address,
-          description,
-          email,
-          capacity,
-          phone,
-        }),
-      })
-      if (!response.ok) {
-        throw new Error("Failed to update restaurant")
-      }
-      alert("Restaurant updated successfully")
+      const update = await updateRestaurant(restaurantData)
+      return { update }
     } catch (error) {
-      console.error(error)
-      alert("Failed to update restaurant")
+      return json({ error: "Failed to update restaurant details" })
     }
   }
 
+  return json({ error: "Invalid method" }, { status: 405 })
+}
+
+export default function RestaurantDetails() {
+  const { restaurant, error } = useLoaderData()
+  const actionData = useActionData()
+  const [name, setName] = useState(restaurant.name)
+  const [phone, setPhone] = useState(restaurant.phone)
+  const [email, setEmail] = useState(restaurant.email)
+  const [address, setAddress] = useState(restaurant.address)
+  const [rating, setRating] = useState(restaurant.rating)
+  const [capacity, setCapacity] = useState(restaurant.capacity)
+  const [description, setDescription] = useState(restaurant.description)
+  const [bookingDuration, setBookingDuration] = useState(
+    restaurant.booking_duration
+  )
+
   if (error) {
-    return <div>{error}</div>
+    return <div className="text-red-500">{error}</div>
   }
 
   return (
-    <div className="min-h-screen flex flex-col relative">
+    <div className="min-h-screen bg-gray-100">
       <Logo />
       <Darkbackground>
-        <div className="absolute top-[120px] left-1/3">
-          <img
-            src={tonniImage} // Use the imported image
-            alt={`${restaurant.name} profile`}
-            className="w-40 h-40 rounded-full object-cover"
-          />
-        </div>
-        <form onSubmit={handleSubmit} className="flex p-8 mt-32 ml-20">
-          <div className="w-full p-4">
-            <div className="h-[100px]">
-              <label className="text-lg font-semibold mb-2">
-                <strong>TITEL</strong>
+        <div className="flex items-center justify-center py-10">
+          <Form
+            method="PUT"
+            className="text-black bg-white p-6 rounded-lg shadow-md w-full max-w-md"
+          >
+            <input type="hidden" name="id" value={restaurant.id} />
+            <div className="mb-4">
+              <label
+                htmlFor="name"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Restaurant Name
               </label>
               <input
+                id="name"
+                name="name"
                 type="text"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
-                className="text-base border w-full bg-transparent border-transparent focus:border-blue-500 focus:ring-0"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md"
               />
             </div>
-            <div className="h-[100px]">
-              <label className="text-lg font-semibold mb-2">
-                <strong>ADDRESSE</strong>
+            <div className="mb-4">
+              <label
+                htmlFor="phone"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Phone
               </label>
               <input
+                id="phone"
+                name="phone"
                 type="text"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-                className="text-base border w-full bg-transparent border-transparent focus:border-blue-500 focus:ring-0"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                className="w-full px-3 py-2 border border-gray-300 rounded-md"
               />
             </div>
-            <div className="h-[100px]">
-              <label className="text-lg font-semibold mb-2">
-                <strong>BESKRIVELSE</strong>
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Email
               </label>
               <input
-                type="text"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                className="text-base border w-full bg-transparent border-transparent focus:border-blue-500 focus:ring-0"
-              />
-            </div>
-          </div>
-          <div className="w-full md:w-1/2 p-4">
-            <div className="h-[100px]">
-              <label className="text-lg font-semibold mb-2">
-                <strong>EMAIL</strong>
-              </label>
-              <input
+                id="email"
+                name="email"
                 type="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="text-base border w-full bg-transparent border-transparent focus:border-blue-500 focus:ring-0"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md"
               />
             </div>
-            <div className="h-[100px]">
-              <label className="text-lg font-semibold mb-2">
-                <strong>KAPACITET</strong>
+            <div className="mb-4">
+              <label
+                htmlFor="address"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Address
               </label>
               <input
+                id="address"
+                name="address"
+                type="text"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                className="w-full px-3 py-2 border border-gray-300 rounded-md"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="rating"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Rating
+              </label>
+              <input
+                id="rating"
+                name="rating"
+                type="number"
+                step="0.1"
+                value={rating}
+                onChange={(e) => setRating(parseFloat(e.target.value))}
+                className="w-full px-3 py-2 border border-gray-300 rounded-md"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="capacity"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Capacity
+              </label>
+              <input
+                id="capacity"
+                name="capacity"
                 type="number"
                 value={capacity}
-                onChange={(e) => setCapacity(e.target.value)}
-                className="text-base border  w-full bg-transparent border-transparent focus:border-blue-500 focus:ring-0"
+                onChange={(e) => setCapacity(parseInt(e.target.value))}
+                className="w-full px-3 py-2 border border-gray-300 rounded-md"
               />
             </div>
-            <div className="h-[100px]">
-              <label className="text-lg font-semibold mb-2">
-                <strong>Phone</strong>
+            <div className="mb-4">
+              <label
+                htmlFor="description"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Description
               </label>
               <input
-                type="tel"
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
-                className="text-base border  w-full bg-transparent border-transparent focus:border-blue-500 focus:ring-0"
+                id="description"
+                name="description"
+                type="text"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                className="w-full px-3 py-2 border border-gray-300 rounded-md"
               />
             </div>
-          </div>
-          <button
-            type="submit"
-            className="mt-4 p-2 bg-blue-500 text-white rounded"
-          >
-            Update
-          </button>
-        </form>
+            <div className="mb-4">
+              <label
+                htmlFor="booking_duration"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Booking Duration
+              </label>
+              <input
+                id="booking_duration"
+                name="booking_duration"
+                type="number"
+                value={bookingDuration}
+                onChange={(e) => setBookingDuration(parseInt(e.target.value))}
+                className="w-full px-3 py-2 border border-gray-300 rounded-md"
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600"
+            >
+              Submit
+            </button>
+          </Form>
+        </div>
       </Darkbackground>
     </div>
   )
