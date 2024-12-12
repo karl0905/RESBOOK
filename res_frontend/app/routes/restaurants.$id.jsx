1c057e8693cdde0ec0406652a0a3b6a224076c81
paulybizz <eaa24carp@students.eaaa.dkgit config --global user.emailgit config --global user.emailgit config --global user.email>
2024-12-12 15:34:40 +0100

    adjust layout in RestaurantDetails component for improved positioning

--- res_frontend/app/routes/restaurants.$id.jsx
+++ res_frontend/app/routes/restaurants.$id.jsx
@@ -71,7 +71,7 @@ export default function RestaurantDetails() {
       <Logo />
       <Darkbackground>
         <div className="flex flex-col items-center justify-center py-10 px-4 sm:px-6 lg:px-8">
-          <div className="relative top-28 sm:top-32 lg:top-36">
+          <div className="relative  mt-[-100px] ">
             <img
               src="/global/assets/ppicture.png"
               alt="Profile"
@@ -80,7 +80,7 @@ export default function RestaurantDetails() {
           </div>
           <Form
             method="PUT"
-            className="mt-36 sm:mt-40 lg:mt-44 text-white bg-transparent p-4 sm:p-6 lg:p-8 rounded-lg shadow-md w-full max-w-md"
+            className=" text-white bg-transparent p-4 sm:p-6 lg:p-8 rounded-lg shadow-md w-full max-w-md"
           >
             <input type="hidden" name="id" value={restaurant.id} />
             <input type="hidden" name="rating" value={rating} />
