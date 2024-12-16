22c408143c162b230fb74dd3d41a40b6e6afcd2a
madsnbkristensen <madskristensen@hotmail.dk>
2024-12-16 10:22:26 +0100

    Updated navbar dead links

--- res_frontend/global/components/Navbar.jsx
+++ res_frontend/global/components/Navbar.jsx
@@ -1,52 +1,52 @@
-import { FaUtensils, FaHeart, FaStore, FaSearch, FaUser } from "react-icons/fa";
-import { useLocation } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { FaUtensils, FaHeart, FaStore, FaSearch, FaUser } from "react-icons/fa"
+import { useLocation } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 export default function Navbar() {
-    const location = useLocation();
-    const path = location.pathname;
+  const location = useLocation()
+  const path = location.pathname
 
-    if (path === "/login" || path === "/signup") {
-        return null;
+  if (path === "/login" || path === "/signup") {
+    return null
+  }
+
+  const getIconColor = (paths) => {
+    if (paths.includes("/") && (path === "/" || path.includes("/restaurant"))) {
+      return "text-green-600"
     }
+    return paths.some((p) => p !== "/" && path.includes(p))
+      ? "text-green-600"
+      : "text-gray-400"
+  }
 
-    const getIconColor = (paths) => {
-        if (paths.includes("/") && (path === "/" || path.includes("/restaurant"))) {
-            return "text-green-600";
-        }
-        return paths.some((p) => p !== "/" && path.includes(p))
-            ? "text-green-600"
-            : "text-gray-400";
-    };
-
-    return (
-        <nav className="fixed bottom-0 w-full py-1 bg-card-dark-gray text-white shadow-lg z-30">
-            <ul className="flex justify-center items-center gap-20 py-2">
-                <li className="flex flex-col items-center">
-                    <Link to="/dashboard" className="flex flex-col items-center w-16">
-                        <FaUtensils
-                            className={`${getIconColor(["/dashboard"])} text-lg sm:text-2xl`}
-                        />
-                        <span className="text-xs sm:text-sm pt-2">Restauranter</span>
-                    </Link>
-                </li>
-                <li className="flex flex-col items-center">
-                    <Link to="/bookings" className="flex flex-col items-center w-16">
-                        <FaStore
-                            className={`${getIconColor(["/bookings"])} text-lg sm:text-2xl`}
-                        />
-                        <span className="text-xs sm:text-sm pt-2">Bookings</span>
-                    </Link>
-                </li>
-                <li className="flex flex-col items-center">
-                    <Link to="/profile" className="flex flex-col items-center w-16">
-                        <FaUser
-                            className={`${getIconColor(["/profile"])} text-lg sm:text-2xl`}
-                        />
-                        <span className="text-xs sm:text-sm pt-2">Profil</span>
-                    </Link>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="fixed bottom-0 w-full py-1 bg-card-dark-gray text-white shadow-lg z-30">
+      <ul className="flex justify-center items-center gap-20 py-2">
+        <li className="flex flex-col items-center">
+          <Link to="/dashboard" className="flex flex-col items-center w-16">
+            <FaUtensils
+              className={`${getIconColor(["/dashboard"])} text-lg sm:text-2xl`}
+            />
+            <span className="text-xs sm:text-sm pt-2">Restauranter</span>
+          </Link>
+        </li>
+        <li className="flex flex-col items-center">
+          <Link to="/bookings" className="flex flex-col items-center w-16">
+            <FaStore
+              className={`${getIconColor(["/bookings"])} text-lg sm:text-2xl`}
+            />
+            <span className="text-xs sm:text-sm pt-2">Bookings</span>
+          </Link>
+        </li>
+        <li className="flex flex-col items-center">
+          <Link to="/#" className="flex flex-col items-center w-16">
+            <FaUser
+              className={`${getIconColor(["/profile"])} text-lg sm:text-2xl`}
+            />
+            <span className="text-xs sm:text-sm pt-2">Profil</span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
 }
